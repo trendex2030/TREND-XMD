ff4c6f05ace974a9b7844ae2092dde8de941fbcf
trendex2030 <bravinnyakundi674@gmail.com>
2025-09-03 17:07:57 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -17,7 +17,7 @@ const menu = async (m, sock) => {
     const seconds = Math.floor(uptimeSeconds % 60);
     const uptime = `${hours}h ${minutes}m ${seconds}s`;
 
-    let profilePictureUrl = 'https://files.catbox.moe/x18hgf.jpg'; 
+    let profilePictureUrl = 'https://files.catbox.moe/j2h8dg.jpg'; 
     try {
       const controller = new AbortController();
       const timeout = setTimeout(() => controller.abort(), 1500); 
@@ -29,225 +29,225 @@ const menu = async (m, sock) => {
     }
 
     const menuText = `
-╭───────────────⭓
-│ 🤖 ʙᴏᴛ : *ᴘᴏᴘᴋɪᴅ-xᴅ*
+╭─── 𝙏𝙍𝙀𝙉𝘿-𝙓 ──────
+│ 🤖 ʙᴏᴛ : *TREND-X*
 │ ⏱️ ʀᴜɴᴛɪᴍᴇ : ${uptime}
 │ ⚡ sᴘᴇᴇᴅ : ${responseTime}s
 │ 🌐 ᴍᴏᴅᴇ : public
 │ 🧩 ᴘʀᴇғɪx : ${prefix}
-│ 👑 ᴏᴡɴᴇʀ : ᴘᴏᴘᴋɪᴅ
+│ 𓅓 ᴏᴡɴᴇʀ : TREND-X
 │ 🛠️ ᴅᴇᴠ : *ᴘᴏᴘᴋɪᴅ*
 │ 🧪 ᴠᴇʀ : *2.0.0*
 ╰───────────────⭓
-━━━━━━━━━━━━━━━━━━
-💥 *𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙋𝙊𝙋𝙆𝙄𝘿-𝙓𝘿* 💥
-━━━━━━━━━━━━━━━━━━
+┏▣ ━━━━━━━━━━━━━━━━━━
+│𓅓 *WE LEAD OTHERS COPY* 𓅓
+┗▣━━━━━━━━━━━━━━━━━━
 
-📜 『 *𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨* 』
-❏ menu
-❏ bugmenu
-❏ speed
-❏ alive
-❏ sudo
-❏ addpremium
-❏ dev
-❏ allvar
-❏ ping
-❏ owner
-
-👑 『 *𝗢𝗪𝗡𝗘𝗥 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦* 』
-❏ join
-❏ autoread
-❏ pair
-❏ leave
-❏ autostatusview
-❏ autotyping
-❏ autoblock
-❏ autorecording
-❏ autosticker
-❏ antisticker
-❏ restart
-❏ block
-❏ unblock
-❏ anticall
-❏ antidelete
-❏ upload
-❏ vv
-❏ setstatusmsg
-❏ allcmds
-❏ calculater
-❏ alwaysonline
-❏ delete
-❏ vv2
-❏ setprefix
-❏ setownername
-❏ profile
-❏ repo
-
-🧠 『 *𝗔𝗜 & 𝗖𝗛𝗔𝗧* 』
-❏ ai
-❏ bug
-❏ bot
-❏ report
-❏ gemini
-❏ chatbot
-❏ gpt
-❏ lydia
-❏ popkid-ai
-
-🎨 『 *𝗖𝗢𝗡𝗩𝗘𝗥𝗧𝗘𝗥𝗦* 』
-❏ security
-❏ sessioncheck
-❏ blockunknown
-❏ autoblock
-❏ host
-❏ antispam
-❏ antibugs
-❏ attp
-❏ gimage
-❏ mp3
-❏ ss
-❏ fancy
-❏ url
-❏ url2
-❏ shorten
-❏ sticker
-❏ take
-
-🔍 『 *𝗦𝗘𝗔𝗥𝗖𝗛 & 𝗧𝗢𝗢𝗟𝗦* 』
-❏ google
-❏ mediafire
-❏ quranvideo
-❏ quraimage
-❏ facebook
-❏ instagram
-❏ tiktok
-❏ lyrics
-❏ ytsearch
-❏ app
-❏ bing
-❏ ipstalk
-❏ imdb
-❏ pinterest
-❏ githubstalk
-❏ image
-❏ ringtone
-❏ playstore
-❏ shazam
-
-🎮 『 *𝗙𝗨𝗡 & 𝗚𝗔𝗠𝗘𝗦* 』
-❏ getpp
-❏ avatar
-❏ wcg
-❏ joke
-❏ ttt
-❏ yesorno
-❏ connect4
-❏ rank
-❏ quizz
-❏ movie
-❏ flirt
-❏ givetext
-❏ roast
-❏ anime
-❏ profile
-❏ ebinary
-❏ fetch
-❏ qc
-❏ couple
-❏ poll
-❏ score
-❏ toqr
-❏ tempmail
-
-👥 『 *𝗚𝗥𝗢𝗨𝗣 𝗖𝗢𝗡𝗧𝗥𝗢𝗟* 』
-❏ kickall
-❏ remove
-❏ tagall
-❏ hidetag
-❏ forward
-❏ getall
-❏ group open
-❏ group close
-❏ add
-❏ vcf
-❏ left
-❏ promoteall
-❏ demoteall
-❏ setdescription
-❏ linkgc
-❏ antilink
-❏ antilink2
-❏ antisticker
-❏ antispam
-❏ create
-❏ setname
-❏ promote
-❏ demote
-❏ groupinfo
-❏ balance
-
-🔞 『 *𝗛𝗘𝗡𝗧𝗔𝗜* 』
-❏ hneko
-❏ trap
-❏ hwaifu
-❏ hentai
-
-🎧 『 *𝗔𝗨𝗗𝗜𝗢 𝗘𝗙𝗙𝗘𝗖𝗧𝗦* 』
-❏ earrape
-❏ deep
-❏ blown
-❏ bass
-❏ nightcore
-❏ fat
-❏ fast
-❏ robot
-❏ tupai
-❏ smooth
-❏ slow
-❏ reverse
-
-💫 『 *𝗥𝗘𝗔𝗖𝗧𝗜𝗢𝗡𝗦* 』
-❏ bonk
-❏ bully
-❏ yeet
-❏ slap
-❏ nom
-❏ poke
-❏ awoo
-❏ wave
-❏ smile
-❏ dance
-❏ smug
-❏ blush
-❏ cringe
-❏ sad
-❏ happy
-❏ shinobu
-❏ cuddle
-❏ glomp
-❏ handhold
-❏ highfive
-❏ kick
-❏ kill
-❏ kiss
-❏ cry
-❏ bite
-❏ lick
-❏ pat
-❏ hug
-
-━━━━━━━━━━━━━━━━━━
-⚡ *POPᴋID GLE V2.0* ⚡
-━━━━━━━━━━━━━━━━━━
+┏▣ 📜 『 *𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨* 』
+│➽ menu
+│➽ bugmenu
+│➽ speed
+│➽ alive
+│➽ sudo
+│➽ addpremium
+│➽ dev
+│➽ allvar
+│➽ ping
+│➽ owner
+┗▣
+┏▣ 『 *𝗢𝗪𝗡𝗘𝗥 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦* 』
+│➽ join
+│➽ autoread
+│➽ pair
+│➽ leave
+│➽ autostatusview
+│➽ autotyping
+│➽ autoblock
+│➽ autorecording
+│➽ autosticker
+│➽ antisticker
+│➽ restart
+│➽ block
+│➽ unblock
+│➽ anticall
+│➽ antidelete
+│➽ upload
+│➽ vv
+│➽ setstatusmsg
+│➽ allcmds
+│➽ calculater
+│➽ alwaysonline
+│➽ delete
+│➽ vv2
+│➽ setprefix
+│➽ setownername
+│➽ profile
+│➽ repo
+┗▣
+┏▣  『 *𝗔𝗜 & 𝗖𝗛𝗔𝗧* 』
+│➽ ai
+│➽ bug
+│➽ bot
+│➽ report
+│➽ gemini
+│➽ chatbot
+│➽ gpt
+│➽ lydia
+│➽ popkid-ai
+┗▣
+┏▣  『 *𝗖𝗢𝗡𝗩𝗘𝗥𝗧𝗘𝗥𝗦* 』
+│➽ security
+│➽ sessioncheck
+│➽ blockunknown
+│➽ autoblock
+│➽ host
+│➽ antispam
+│➽ antibugs
+│➽ attp
+│➽ gimage
+│➽ mp3
+│➽ ss
+│➽ fancy
+│➽ url
+│➽ url2
+│➽ shorten
+│➽ sticker
+│➽ take
+┗▣
+┏▣ 『 *𝗦𝗘𝗔𝗥𝗖𝗛 & 𝗧𝗢𝗢𝗟𝗦* 』
+│➽ google
+│➽ mediafire
+│➽ quranvideo
+│➽ quraimage
+│➽ facebook
+│➽ instagram
+│➽ tiktok
+│➽ lyrics
+│➽ ytsearch
+│➽ app
+│➽ bing
+│➽ ipstalk
+│➽ imdb
+│➽ pinterest
+│➽ githubstalk
+│➽ image
+│➽ ringtone
+│➽ playstore
+│➽ shazam
+┗▣
+┏▣ 『 *𝗙𝗨𝗡 & 𝗚𝗔𝗠𝗘𝗦* 』
+│➽ getpp
+│➽ avatar
+│➽ wcg
+│➽ joke
+│➽ ttt
+│➽ yesorno
+│➽ connect4
+│➽ rank
+│➽ quizz
+│➽ movie
+│➽ flirt
+│➽ givetext
+│➽ roast
+│➽ anime
+│➽ profile
+│➽ ebinary
+│➽ fetch
+│➽ qc
+│➽ couple
+│➽ poll
+│➽ score
+│➽ toqr
+│➽ tempmail
+┗▣
+┏▣ 『 *𝗚𝗥𝗢𝗨𝗣 𝗖𝗢𝗡𝗧𝗥𝗢𝗟* 』
+│➽ kickall
+│➽ remove
+│➽ tagall
+│➽ hidetag
+│➽ forward
+│➽ getall
+│➽ group open
+│➽ group close
+│➽ add
+│➽ vcf
+│➽ left
+│➽ promoteall
+│➽ demoteall
+│➽ setdescription
+│➽ linkgc
+│➽ antilink
+│➽ antilink2
+│➽ antisticker
+│➽ antispam
+│➽ create
+│➽ setname
+│➽ promote
+│➽ demote
+│➽ groupinfo
+│➽ balance
+┗▣
+┏▣ 『 *𝗛𝗘𝗡𝗧𝗔𝗜* 』
+│➽ hneko
+│➽ trap
+│➽ hwaifu
+│➽ hentai
+┗▣
+┏▣ 『 *𝗔𝗨𝗗𝗜𝗢 𝗘𝗙𝗙𝗘𝗖𝗧𝗦* 』
+│➽ earrape
+│➽ deep
+│➽ blown
+│➽ bass
+│➽ nightcore
+│➽ fat
+│➽ fast
+│➽ robot
+│➽ tupai
+│➽ smooth
+│➽ slow
+│➽ reverse
+┗▣
+┏▣ 『 *𝗥𝗘𝗔𝗖𝗧𝗜𝗢𝗡𝗦* 』
+│➽ bonk
+│➽ bully
+│➽ yeet
+│➽ slap
+│➽ nom
+│➽ poke
+│➽ awoo
+│➽ wave
+│➽ smile
+│➽ dance
+│➽ smug
+│➽ blush
+│➽ cringe
+│➽ sad
+│➽ happy
+│➽ shinobu
+│➽ cuddle
+│➽ glomp
+│➽ handhold
+│➽ highfive
+│➽ kick
+│➽ kill
+│➽ kiss
+│➽ cry
+│➽ bite
+│➽ lick
+│➽ pat
+│➽ hug
+│
+┗▣━━━━━━━━━━━━━━━━━━
+│𓅓 *TREND-X* 𓅓
+┗▣━━━━━━━━━━━━━━━━━━
     `.trim();
 
     const newsletterContext = {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterName: "Popkid-Gle",
-        newsletterJid: "120363420342566562@newsletter"
+        newsletterName: "TREND-X",
+        newsletterJid: "120363401765045963@newsletter"
       }
     };
 
