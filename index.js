938695bf5e0da78f0340365dbedfc36423f44563
trendex2030 <bravinnyakundi674@gmail.com>
2025-09-23 19:01:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -219,11 +219,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-// Anti-crash handler
-process.on("uncaughtException", (err) => {
-  console.error("[❗] Uncaught Exception:", err.stack || err);
-});
-
-process.on("unhandledRejection", (reason, p) => {
-  console.error("[❗] Unhandled Promise Rejection:", reason);
-});
