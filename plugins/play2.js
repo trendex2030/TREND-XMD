ffea199e665caf18dac53c4c57001181e8c6a2ef
trendex2030 <bravinnyakundi674@gmail.com>
2025-09-25 13:52:21 +0300

    Update play2.js

--- plugins/play2.js
+++ plugins/play2.js
@@ -25,7 +25,7 @@ const play2 = async (m, gss) => {
         videoUrl = searchResults.videos[0].url;
       }
 
-      const apiUrl = `https://api.davidcyriltech.my.id/youtube/mp3?url=${encodeURIComponent(videoUrl)}`;
+      const apiUrl = `https://apis.davidcyriltech.my.id/play?query=${encodeURIComponent(videoUrl)}`;
       const { data } = await axios.get(apiUrl);
 
       if (!data.success) return m.reply("❌ Failed to download audio");
